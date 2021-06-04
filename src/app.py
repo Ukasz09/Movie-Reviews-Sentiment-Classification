7d12380ff5670abae33504653224db5758df0004
ukasz09 <246703@student.pwr.edu.pl>
2021-06-04 17:01:34 +0200

    Add k_cross_fold

--- src/app.py
+++ src/app.py
@@ -1,13 +1,11 @@
 from src.dataAnalysis import *
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold
 from sklearn.naive_bayes import MultinomialNB
 from sklearn import metrics
 
 negative_sentiment = 0
 neutral_sentiment = 1
 positive_sentiment = 2
-
-test_size = 0.2
 
 
 def read_data() -> Tuple[List[str], List[int]]:
@@ -32,17 +30,37 @@ def predict_with_bayes(X_train, X_test, y_train):
     return mnb.predict(X_test)
 
 
+def split_normal(test_size=0.2):
+    X_train_raw, X_test_raw, y_train, y_test = train_test_split(X_raw, y_raw, test_size=test_size)
+    return X_train_raw, X_test_raw, y_train, y_test
+
+
+def get_from_raw(raw, indexes):
+    data = []
+    for index in indexes:
+        data.append(raw[index])
+    return data
+
+
 if __name__ == "__main__":
     cv = CountVectorizer(stop_words='english')
     X_raw, y_raw = read_data()
-    X_train_raw, X_test_raw, y_train, y_test = train_test_split(X_raw, y_raw, test_size=test_size)
-    X_train = cv.fit_transform(X_train_raw)
-    X_test = cv.transform(X_test_raw)
 
-    y_predicted = predict_with_bayes(X_train, X_test, y_train)
-
-    accuracy_score = metrics.accuracy_score(y_predicted, y_test)
-    print(str('{:04.2f}'.format(accuracy_score * 100)) + '%')
+    k_fold_groups = 10
+    k_fold = KFold(k_fold_groups, shuffle=True)
+    accuracy_sum = 0
+    i = 0
+    for train_indexes, test_indexes in k_fold.split(X_raw):
+        X_train_raw, X_test_raw = get_from_raw(X_raw, train_indexes), get_from_raw(X_raw, test_indexes)
+        y_train, y_test = get_from_raw(y_raw, train_indexes), get_from_raw(y_raw, test_indexes)
+        X_train, X_test = cv.fit_transform(X_train_raw), cv.transform(X_test_raw)
+        y_predicted = predict_with_bayes(X_train, X_test, y_train)
+        accuracy_score = metrics.accuracy_score(y_predicted, y_test)
+        accuracy_sum += accuracy_score
+        i += 1
+        print('Fold=', i, str('Mean accuracy = {:04.2f}'.format(accuracy_score * 100)), '%')
+    mean_accuracy = accuracy_sum / k_fold_groups
+    print('\n', str('Mean accuracy = {:04.2f}'.format(mean_accuracy * 100)) + '%')
 
     # Dataset analysis
     # count_words_all()
