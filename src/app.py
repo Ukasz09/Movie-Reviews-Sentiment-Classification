cf4b1ed19001538d896b09c98b8e937ca4d6c3c7
ukasz09 <246703@student.pwr.edu.pl>
2021-06-04 16:00:12 +0200

    Change input for countVecotrizer

--- src/app.py
+++ src/app.py
@@ -1,48 +1,44 @@
 from typing import *
 from src.dataAnalysis import *
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
 
 negative_sentiment = 0
 neutral_sentiment = 1
 positive_sentiment = 2
 
-train_size = 0.8
 test_size = 0.2
 
 
-def get_data() -> Dict[str, int]:
+def read_data() -> Tuple[List[str], List[int]]:
     """
     :return: Dictionary (sentence, sentiment)
     """
     reviews_per_author = read_reviews()
     labels_per_author = read_labels()
-    data = {}
+    all_reviews = []
+    all_sentiments = []
     for author in reviews_per_author.keys():
         reviews = reviews_per_author[author]
         labels = labels_per_author[author]
-        for r, l in zip(reviews, labels):
-            data[r] = l
-    return data
-
-
-def get_vectorizer(reviews_list):
-    vectorizer = CountVectorizer()
-    vectorizer.fit(reviews_list)
-    return vectorizer
+        all_reviews += reviews
+        all_sentiments += labels
+    return all_reviews, all_sentiments
 
 
 if __name__ == "__main__":
-    # CountVectorizer
-    reviews_per_author = read_reviews()
-    all_reviews = sum(reviews_per_author.values(), [])
-    data = get_vectorizer(all_reviews)
-    vocabulary = data.vocabulary_
+    X, Y = read_data()
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=test_size)
+    cv = CountVectorizer(stop_words='english')
+    cvFitTrans = cv.fit_transform(X_train)
+    cvTestTrans = cv.transform(X_test)
 
     # Dataset analysis
-    count_words_all()
-    count_words_per_author()
-    calc_length_all()
-    calc_length_per_author()
-    count_labels_all()
-    count_labels_per_author()
-    words_qty_per_sentence_all()
-    words_qty_per_sentence_and_author()
+    # count_words_all()
+    # count_words_per_author()
+    # calc_length_all()
+    # calc_length_per_author()
+    # count_labels_all()
+    # count_labels_per_author()
+    # words_qty_per_sentence_all()
+    # words_qty_per_sentence_and_author()
